68e2a93d8f18eb4120813d38a309627025e140b6
PMartn <71846491+PMartn@users.noreply.github.com>
2026-06-19 19:54:30 +0200

    FEAT: TypeORM postgres DB

---
+++ skeleton/src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfigService } from '../config/app-config.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [AppConfigService],
+      useFactory: (configService: AppConfigService) => ({
+        type: 'postgres',
+        url: configService.databaseUrl,
+        autoLoadEntities: true,
+        synchronize: true, // TODO: Add option to toggle this
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
